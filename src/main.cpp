bb62150da15bf50138cac21c57d1bc1a39f86e0d
Jonathan <jonathanadalin@gmail.com>
2019-01-19 10:42:08 -0600

    Updated .gitignore, fixed formatting

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,24 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
-	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
-		return -1;
-	}
+  if (argc != 2)
+  {
+    cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+    return -1;
+  }
 
-	Mat image;
-	image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+  Mat image;
+  image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-	if (!image.data)
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
-	}
+  if (!image.data)
+  {
+    cout << "Could not open or find the image" << std::endl;
+    return -1;
+  }
 
-	namedWindow("Display window", WINDOW_AUTOSIZE);
-	imshow("Display window", image);
+  namedWindow("Display window", WINDOW_AUTOSIZE);
+  imshow("Display window", image);
 
-	waitKey(0);
-	return 0;
+  waitKey(0);
+  return 0;
 }
